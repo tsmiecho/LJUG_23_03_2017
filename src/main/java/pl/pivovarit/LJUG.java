adacb8ec1b9885c43c33b8a7ba710f388949537d
pivovarit <gpiwowarek@gmail.com>
2017-03-23 15:15:16 +0100

    Initial

---
+++ src/main/java/pl/pivovarit/LJUG.java
@@ -0,0 +1,15 @@
+package pl.pivovarit;
+
+
+/**
+ * Grzegorz Piwowarek
+ * gpiwowarek@gmail.com
+ * @pivovarit
+ * https://github.com/pivovarit/LJUG_23_03_2017
+ */
+public class LJUG {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Hello");
+    }
+}
---
+++ src/test/java/pl/pivovarit/javaslang/S00_Javaslang_Test.java
@@ -0,0 +1,10 @@
+package pl.pivovarit.javaslang;
+
+/**
+ * Grzegorz Piwowarek
+ * gpiwowarek@gmail.com
+ * @pivovarit
+ * https://github.com/pivovarit/LJUG_23_03_2017
+ */
+public class S00_Javaslang_Test {
+}
